b485a38f5a4c62bfa875654704f431789b26bf81
boo_manKnow <ukyokyo408shishido@gmail.com>
2026-08-01 02:35:17 +0900

    feat: 可視化表示の切り替え機能を追加

--- src/components/FileTree.jsx
+++ src/components/FileTree.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
-import { Tree } from 'antd';
+import React, { Children } from 'react';
+import { Tree, Tabs } from 'antd';
 
 export default function FileTree({ treeData, onSelect }) {
+    const onChange = key => {
+        console.log(key);
+    };
+
+    const items = [
+        {
+            key: '1',
+            label: 'ツリー表示',
+            children: <p>a</p>
+        },
+        {
+            key: '2',
+            label: 'ファイル表示',
+            children: <Tree
+                showLine
+                defaultExpandAll={true}
+                onSelect={onSelect}
+                treeData={treeData}
+                titleRender={(node) => (
+                    <span style={node.data?.status === 'deleted' ? { textDecoration: 'line-through', opacity: 0.5 } : undefined}>
+                        {node.title}
+                    </span>
+                )}
+            />
+        }
+    ];
+    
     return (
-        <Tree
-            showLine
-            defaultExpandAll={true}
-            onSelect={onSelect}
-            treeData={treeData}
-            titleRender={(node) => (
-                <span style={node.data?.status === 'deleted' ? { textDecoration: 'line-through', opacity: 0.5 } : undefined}>
-                    {node.title}
-                </span>
-            )}
-        />
+        <>
+            <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
+        </>
     );
 }
